e8f00da801d9ecee9bc057c5d40f919ff535292a
Slava30041991 <balahenka30041991@gmail.com>
2024-01-07 21:46:37 +0300

    lesson5 добавлен класс Plate

--- src/main/java/lesson6/Table.java
+++ src/main/java/lesson6/Table.java
@@ -1,15 +1,17 @@
 package lesson6;
+
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-@JsonPropertyOrder({"Value1", "Value2","Value1"})
+@JsonPropertyOrder({"Value1", "Value2", "Value1"})
 class Number {
     public int Value1;
     public int Value2;
@@ -27,15 +29,12 @@ public class Table {
     public static void main(String[] args) throws IOException {
         List<Number> numbers = new ArrayList<>();
         numbers.add(new Number(100, 200, 123));
-        numbers.add(new Number(300, 400,500));
+        numbers.add(new Number(300, 400, 500));
 
 
         CsvMapper mapper = new CsvMapper();
-        CsvSchema schema = mapper.schemaFor(Number.class)
-                .withColumnSeparator(';')
-                .withoutQuoteChar()
-                .withHeader();
+        CsvSchema schema = mapper.schemaFor(Number.class).withColumnSeparator(';').withoutQuoteChar().withHeader();
         ObjectWriter writer = mapper.writer(schema);
-        writer.writeValue(new FileWriter("test.csv", StandardCharsets.UTF_8),numbers);
+        writer.writeValue(new FileWriter("test.csv", StandardCharsets.UTF_8), numbers);
     }
 }
